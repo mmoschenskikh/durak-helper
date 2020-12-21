671706b992f7a6b11439e117e04bd5783edb272c
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-12-21 18:45:41 +0300

    Card representation updated

--- app/src/main/java/ru/maxultra/durakhelper/model/Card.kt
+++ app/src/main/java/ru/maxultra/durakhelper/model/Card.kt
@@ -1,11 +1,23 @@
 package ru.maxultra.durakhelper.model
 
-data class Card(val suit: Suit, val rank: Rank) {
-    enum class Suit(val suitImage: String) {
+data class Card(val suit: Suit, val rank: Rank, var status: Status = Status.TABLE) {
+    enum class Suit(val suitString: String) {
         DIAMONDS("♦"), CLUBS("♣"), HEARTS("♥"), SPADES("♠")
     }
 
-    enum class Rank {
-        ACE, KING, QUEEN, JACK, TEN, NINE, EIGHT, SEVEN, SIX
+    enum class Rank(val rankString: String) {
+        ACE("A"), KING("K"), QUEEN("Q"), JACK("J"),
+        TEN("10"), NINE("9"), EIGHT("8"), SEVEN("7"), SIX("6")
+    }
+
+    /**
+     * Describes who owns the card
+     */
+    enum class Status {
+        TABLE, USER, FRIEND, ENEMY, DISCARD
+    }
+
+    override fun toString(): String {
+        return rank.rankString + suit.suitString
     }
 }
