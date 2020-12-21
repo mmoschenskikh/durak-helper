1fe0d1df6e637a6c300b39e7064aadf931496f0d
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-12-21 21:46:30 +0300

    Fragment placed to MainActivity

--- app/src/main/java/ru/maxultra/durakhelper/MainActivity.kt
+++ app/src/main/java/ru/maxultra/durakhelper/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = DeckFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
